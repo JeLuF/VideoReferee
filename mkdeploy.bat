1e56f8573088d4096910f0c62336ecd461f7251f
JeLuF <jf@mormo.org>
2026-03-14 01:28:28 +0100

    add Qt file

--- mkdeploy.bat
+++ mkdeploy.bat
@@ -1,5 +1,8 @@
 mkdir deploy
 copy release\VideoReferee.exe deploy
-windeployqt --release release\VideoReferee.exe
+windeployqt --release deploy\VideoReferee.exe
 
-copy release\libgcc_s_seh-1.dll release\libstdc++-6.dll release\opencv_videoio_ffmpeg4120_64.dll deploy\
+copy "release\libgcc_s_seh-1.dll" deploy\
+copy "release\libstdc++-6.dll" deploy\
+copy "release\opencv_videoio_ffmpeg4120_64.dll" deploy\
+copy "release\libopencv_world4120.dll" deploy\
